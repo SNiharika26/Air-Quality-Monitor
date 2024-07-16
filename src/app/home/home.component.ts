0f9ac5c4c5ed10b02d99fc5816c572dce4f23673
Santhoshi Chaluvadi (TMNA) <santhoshi.chaluvadi@toyota.com>
2024-07-16 22:04:49 +0530

    Home component code

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { AirPollutionService } from '../air-pollution.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {}
